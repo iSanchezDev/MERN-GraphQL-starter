6766ace7283ec2f271c8b48db83df8eed656c584
Iván Sánchez <isancheznetwork@gmail.com>
2019-02-09 15:59:12 +0100

    feat(app): Login (no actions provided)

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import './Home.css';
+import './home.css';
+import Login from './../login/login';
 
 class Home extends Component {
   render() {
-    return (<h1>Home</h1>);
+    return (
+      <div>
+        <Login/>
+      </div>
+    );
   }
 }
 
