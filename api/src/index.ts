13e03699707fb35070a9f7a4e584bae782cb4a0f
Iván Sánchez <isancheznetwork@gmail.com>
2019-02-10 19:35:58 +0100

    feat(api): Middleware integration

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,17 +5,11 @@ import schema from './schema'
 import config from './config'
 import graphqlHTTP from 'express-graphql'
 import mongoose from 'mongoose';
-import authRoutes from './routes/auth.routes'
-import { verifyToken } from './controllers/auth/auth.controller';
+import authRoutes from './routes/auth.routes';
+import graphqlMiddleware from './middleware/graphql.middleware';
 
 const port = process.env.PORT || 3001;
 const dev = process.env.NODE_ENV === 'development';
-
-/*
- * Express config
- */
-const app = express();
-app.use(cors());
 
 /**
  * Mongodb database connection
@@ -26,27 +20,21 @@ mongoose.connect(
 );
 
 /*
- * Place your custom routes here
+ * Express config
+ */
+const app = express();
+app.use(cors());
+
+/*
+ * Customize your routes
  */
 app.use('/auth', authRoutes);
 
+
 /*
- * Auth middleware
+ * GraphQL using express
  */
-app.use('/graphql', async (req, res, next) => {
-  const token = req.headers.authorization;
-  try {
-    const user: any = await verifyToken(token);
-    if (user) {
-      req.user = user;
-      next()
-    }
-  } catch (e) {
-    res.status(401).json({
-      message: e.message
-    })
-  }
-});
+app.use(graphqlMiddleware);
 
 app.use('/graphql', graphqlHTTP((req, res) => ({
     schema,
@@ -56,7 +44,6 @@ app.use('/graphql', graphqlHTTP((req, res) => ({
     }
   }))
 );
-
 
 app.listen(port,  () => {
   console.log(`🔥🚀 Server ready at http://localhost:${port}`);
